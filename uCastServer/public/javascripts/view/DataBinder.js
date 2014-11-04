373219a760d5c77cb5ed20ff9283699d11188c1d
rogerpereira123 <rogerpereira123@gmail.com>
2014-11-03 22:25:38 -0500

    uCast media queue controls

--- uCastServer/public/javascripts/view/DataBinder.js
+++ uCastServer/public/javascripts/view/DataBinder.js
@@ -1,7 +1,8 @@
 ﻿define(["jquery" ,  "underscore" , "datatables" , ], function ($, _, dt) { 
     $.bindFiles = function (arrFiles , castMe) {
         $('.files').html('<table id="tblFiles" class="display" cellspacing="0" width="100%"></table>');
-        var data = "<thead><tr><th align='left' valign='bottom'><input type='checkbox' id='chkSelectAll'> File Name</th></tr></thead>";
+        var data = "<thead><tr><th align='left' valign='top' style='height:24px;'>";
+           data +="<input type='checkbox' name='chkSelectAll' id='chkSelectAll' /><label for='chkSelectAll'> File Name</label> </th></tr></thead>";
         $('#tblFiles').append(data);
         var d = _.map(arrFiles , function (f) { return [f.FileName]; });
         $('#tblFiles').dataTable({
@@ -12,12 +13,30 @@
             "bInfo": false,
             "bPaginate": false,
             "fnRowCallback": function (nRow, aData, iDisplayIndex) {
-                $('td:eq(0)' , nRow).html('<a class="GridLink click">' + aData[0] + '</a>');
+                $('td:eq(0)' , nRow).html('<input type="checkbox" class="selectFile" id="chk' + iDisplayIndex + '" /><a class="GridLink click">' + aData[0] + '</a>');
                 
                 //$('td:eq(0)' , nRow).append('hi pg' + iDisplayIndex + '');
                 $('.click', nRow).css('cursor', 'pointer');
                 $('.click', nRow).click(function () { castMe(aData[0], iDisplayIndex); });
                     
+            }
+        });
+        
+        $("#playselected").hide();
+        $("#playselected").attr("src" , "../images/playselected.png");
+        //$("#playselected").button();
+
+
+        $("#chkSelectAll").click(function () {
+            if ($("#chkSelectAll").is(":checked")) {
+                $(".selectFile").prop("checked" , true);
+                $('.headerFiles').html("Play Selected: ");
+                $("#playselected").show();
+            }
+            else {
+                $(".selectFile").prop("checked" , false);
+                $('.headerFiles').html("Following files found");
+                $("#playselected").hide();
             }
         });
             
